1a0513bfd01938ab2b10fc8b23c91768c1ceb6bc
mekh <smajev@protonmail.com>
2026-05-07 19:33:53 +0200

    Room model added

---
+++ RoomManager/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomManager.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string BuildingCode { get; set; }
+    
+    public int Floor { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "You must select a floor")];
+    public int Capacity { get; set; }
+    
+    public bool HasProjector { get; set; }
+    
+    public bool IsActive { get; set; }
+}
