edd835e8f9b882c802f185948217c2bcf4eccaf8
mekh <smajev@protonmail.com>
2026-05-07 20:52:37 +0200

    getRomms created

--- RoomManager/Controllers/RoomsController.cs
+++ RoomManager/Controllers/RoomsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RoomManager.Data;
 using RoomManager.Models;
 
 namespace RoomManager.Controllers;
@@ -14,6 +15,15 @@ public class RoomsController : ControllerBase
         [FromQuery] bool? activeOnly
     )
     {
+        var rooms = InMemoryDB.Rooms.AsQueryable();
         
+        if (minCapacity.HasValue) rooms = rooms.Where(r => r.Capacity >= minCapacity.Value);
+        if (hasProjector.HasValue) rooms = rooms.Where(r => r.HasProjector == hasProjector.Value);
+        if (activeOnly == true) rooms.Where(r => r.IsActive == true);
+        
+        return Ok(rooms.ToList());
     }
+    
+    
+    
 }
