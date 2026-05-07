2a9f3b9e78552aa83b1d8ff6ff688d8ec60791d5
mekh <smajev@protonmail.com>
2026-05-07 20:42:12 +0200

    Room controller init

--- RoomManager/Controllers/RoomsController.cs
+++ RoomManager/Controllers/RoomsController.cs
@@ -1,6 +1,19 @@
-﻿namespace RoomManager.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using RoomManager.Models;
 
-public class RoomsController
+namespace RoomManager.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
 {
-    
+    [HttpGet]
+    public ActionResult<List<Room>> GetRooms(
+        [FromQuery] int? minCapacity,
+        [FromQuery] bool? hasProjector,
+        [FromQuery] bool? activeOnly
+    )
+    {
+        
+    }
 }
