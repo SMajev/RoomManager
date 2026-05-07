23aba02c37e1da62818b96c4e825c2abad38f73b
mekh <smajev@protonmail.com>
2026-05-07 20:32:27 +0200

    Reservation model added

---
+++ RoomManager/Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomManager.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    
+    public int RoomId { get; set; }
+    
+    [Required]
+    public string OrganizerName { get; set; }
+
+    [Required]
+    public string Topic  { get; set; }
+    
+    public DateOnly Date { get; set; }
+    
+    public TimeOnly StartTime { get; set; }
+    
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; } = "planned";
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime must be later than StartTime.",
+                new[] { nameof(EndTime), nameof(StartTime) }
+            );
+        }
+    }
+}
